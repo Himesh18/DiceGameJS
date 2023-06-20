f9b704d24ea2f916b116a63ca291c67d9e4d4dd8
Himesh_Kumar <77400961+Himesh18@users.noreply.github.com>
2023-06-20 19:35:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var imageSource1 = "./Images/dice" + randomNumber1 + ".png";
+var imageSource2 = "./Images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", imageSource1);
+
+document.querySelector(".img2").setAttribute("src", imageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
